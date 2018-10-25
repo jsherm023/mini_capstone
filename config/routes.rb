70a9ddc4f679c8b057c4e6d4c6b09fdddf04358f
“Joe <joseph.sherman@Loop.colum.edu>
2018-10-24 19:03:36 -0500

    add JSON web tokens

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-   get '/products' => 'products#index'
-   get '/products' => 'products#show'
-   post '/products' => 'products#create'
-   patch '/products:id' => 'products#update'
-   post '/users' => 'users#create'
+    get '/products' => 'products#index'
+    get '/products' => 'products#show'
+    post '/products' => 'products#create'
+    patch '/products:id' => 'products#update'
+    post '/users' => 'users#create'
+    post '/sessions' => 'sessions#create'
   end
 end
 
