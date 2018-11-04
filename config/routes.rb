0b5b420596b941d4d28e3a09831e21e47e397611
“Joe <joseph.sherman@Loop.colum.edu>
2018-11-04 11:56:12 -0600

    Add index, show, new and create action for full stack rails/html.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get '/products' => 'products#index'
+    get 'products' => 'products#index'
+    get '/products/new' => 'products#new'
     get '/products/:id' => 'products#show'
     post '/products' => 'products#create'
-    patch '/products/:id' => 'products#update'
-    delete '/products/:id' => 'products#destroy'
+    namespace :api do
+      get '/products' => 'products#index'
+      get '/products/:id' => 'products#show'
+      post '/products' => 'products#create'
+      patch '/products/:id' => 'products#update'
+      delete '/products/:id' => 'products#destroy'
 
-    post '/users' => 'users#create'
-    post '/sessions' => 'sessions#create'
+      post '/users' => 'users#create'
+      post '/sessions' => 'sessions#create'
 
-    post '/orders' => 'orders#create'
-    get '/orders' => 'orders#index'
+      post '/orders' => 'orders#create'
+      get '/orders' => 'orders#index'
 
-    post '/CartedProducts' => 'carted_products#create'
-    get '/CartedProducts/:id' => 'carted_products#show'
-    get '/CartedProducts/' => 'carted_products#index'
-    delete '/CartedProducts/:id' => 'carted_products#destroy'
-  end
+      post '/CartedProducts' => 'carted_products#create'
+      get '/CartedProducts/:id' => 'carted_products#show'
+      get '/CartedProducts/' => 'carted_products#index'
+      delete '/CartedProducts/:id' => 'carted_products#destroy'
+    end
 end
 
 # product3 = Product.new(name: "megacloth", price: 50, image_url: "www.image.com", description: "the best")
