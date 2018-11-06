49d4bdc7a85e7161e4d51b9c48e8c06cce9c16a5
“Joe <joseph.sherman@Loop.colum.edu>
2018-11-05 19:29:58 -0600

    add edit, update and destroy actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,4 +23,25 @@ class ProductsController < ApplicationController
     @product.save
     redirect_to "/products"
   end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:title]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @supplier_id = params[:supplier_id]
+    @image_id = params[:image_id]
+    redirect_to "/products"
+  end
+  
+  def destroy
+    @product = Product.find_by(id: paramss[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,29 @@
 Rails.application.routes.draw do
-    get 'products' => 'products#index'
-    get '/products/new' => 'products#new'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
+  get '/products/:id' => 'products#show'
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id' => 'products#update'
+  delete '/products/:id' => 'products#destroy'
+  namespace :api do
+    get '/products' => 'products#index'
     get '/products/:id' => 'products#show'
     post '/products' => 'products#create'
-    namespace :api do
-      get '/products' => 'products#index'
-      get '/products/:id' => 'products#show'
-      post '/products' => 'products#create'
-      patch '/products/:id' => 'products#update'
-      delete '/products/:id' => 'products#destroy'
+    patch '/products/:id' => 'products#update'
+    delete '/products/:id' => 'products#destroy'
 
-      post '/users' => 'users#create'
-      post '/sessions' => 'sessions#create'
+    post '/users' => 'users#create'
+    post '/sessions' => 'sessions#create'
 
-      post '/orders' => 'orders#create'
-      get '/orders' => 'orders#index'
+    post '/orders' => 'orders#create'
+    get '/orders' => 'orders#index'
 
-      post '/CartedProducts' => 'carted_products#create'
-      get '/CartedProducts/:id' => 'carted_products#show'
-      get '/CartedProducts/' => 'carted_products#index'
-      delete '/CartedProducts/:id' => 'carted_products#destroy'
-    end
+    post '/carted_products' => 'carted_products#create'
+    get '/carted_products/:id' => 'carted_products#show'
+    get '/carted_products/' => 'carted_products#index'
+    delete '/carted_products/:id' => 'carted_products#destroy'    
+  end
 end
 
 # product3 = Product.new(name: "megacloth", price: 50, image_url: "www.image.com", description: "the best")
